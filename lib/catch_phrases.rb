feb429cee9da067c0e6fd956593551a90a7d9463
Daniel Urbina <danielur1999@gmail.com>
2020-02-04 03:48:08 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -8,3 +8,8 @@ end
 def toadstool
   puts status = "Thank You Mario! But Our Princess Is In Another Castle!"
 end
+
+def link
+  phrase = "It's Dangerous to Go Alone! Take This."
+  puts phrase
+end
